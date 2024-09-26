4cacc48fa8b0541d5904b201dd6bf42c11a938bc
arturxdking <victorxdking@gmail.com>
2024-09-26 12:35:15 -0300

    (26/09) Ajustes no front-end para trabalhar com o back-end

--- front-end/src/pages/car/CarList.jsx
+++ front-end/src/pages/car/CarList.jsx
@@ -1,191 +1,174 @@
-import AddBoxIcon from '@mui/icons-material/AddBox'
-import DeleteForeverIcon from '@mui/icons-material/DeleteForever'
-import EditIcon from '@mui/icons-material/Edit'
-import Box from '@mui/material/Box'
-import Button from '@mui/material/Button'
-import IconButton from '@mui/material/IconButton'
-import Paper from '@mui/material/Paper'
-import Typography from '@mui/material/Typography'
-import { DataGrid } from '@mui/x-data-grid'
 import React from 'react'
-import { Link } from 'react-router-dom'
+import Typography from '@mui/material/Typography'
+import Box from '@mui/material/Box'
+import { DataGrid } from '@mui/x-data-grid'
 import myfetch from '../../lib/myfetch'
+import IconButton from '@mui/material/IconButton'
+import { Link } from 'react-router-dom'
+import EditIcon from '@mui/icons-material/Edit'
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever'
+import Button from '@mui/material/Button'
+import Paper from '@mui/material/Paper'
+import AddBoxIcon from '@mui/icons-material/AddBox'
+//import Waiting from '../../ui/Waiting'
 import useConfirmDialog from '../../ui/useConfirmDialog'
 import useNotification from '../../ui/useNotification'
 import useWaiting from '../../ui/useWaiting'
 
-export default function CarList() {
-  const columns = [
-    {
-      field: 'id',
-      headerName: 'Cód.',
-      type: 'number',
-      width: 80,
-    },
-    {
-      field: 'brand',
-      headerName: 'Marca/Modelo',
-      width: 200,
-      // Colocando dois campos na mesma célula
-      valueGetter: (value, row) => row.brand + '/' + row.model,
-    },
-    {
-      field: 'color',
-      headerName: 'Cor do carro',
-      width: 150,
-    },
+export default function CustomerList() {
 
-    {
-      field: 'year_manufacture',
-      headerName: 'Ano de fabricação',
-      width: 150,
+  const columns = [
+    { 
+      field: 'id', 
+      headerName: 'Cód.', 
+      width: 70,
+      type: "number"
     },
     {
-      field: 'imported',
-      headerName: 'Importado?',
-      width: 100,
-      renderCell: (value) => (value.row.imported ? 'SIM' : ''),
+      field: 'name',
+      headerName: 'Nome',
+      width: 250
     },
     {
-      field: 'plates',
-      headerName: 'Placa',
-      width: 100,
+      field: 'ident_document',
+      headerName: 'CPF',
+      width: 150
     },
     {
-      field: 'selling_date',
-      headerName: 'Data de venda',
-      width: 120,
-      valueFormatter: (value) =>
-        value ? new Date(value).toLocaleDateString('pt-BR') : '',
+      field: 'municipality',
+      headerName: 'Município/UF',
+      width: 200,
+      valueGetter: (value, row) => value + '/' + row.state
     },
     {
-      field: 'selling_price',
-      headerName: 'Preço de venda',
-      width: 120,
-      // valueFormatter: (params) =>
-      //   params.selling_price?.toLocaleString('pt-BR', {
-      //     style: 'currency',
-      //     currency: 'BRL',
-      //   }),
-      valueFormatter: (value) =>
-        value
-          ? new Intl.NumberFormat('pt-BR', {
-              style: 'currency',
-              currency: 'BRL',
-            }).format(value)
-          : '',
+      field: 'phone',
+      headerName: 'Tel./Celular',
+      width: 160
+    },
+    {
+      field: 'email',
+      headerName: 'E-mail',
+      width: 200
     },
     {
       field: '_edit',
       headerName: 'Editar',
       headerAlign: 'center',
       align: 'center',
-      sortable: 'false',
       width: 90,
-      renderCell: (params) => (
-        <Link to={`./${params.id}`}>
-          <IconButton aria-label='Editar'>
+      sortable: false,
+      renderCell: params => (
+        <Link to={'./' + params.id}>
+          <IconButton aria-label="Editar">
             <EditIcon />
           </IconButton>
         </Link>
-      ),
+      )
     },
     {
       field: '_delete',
       headerName: 'Excluir',
       headerAlign: 'center',
       align: 'center',
-      sortable: 'false',
       width: 90,
-      renderCell: (params) => (
-        <IconButton
-          aria-label='Excluir'
-          onClick={() => handleDeleteButtonClick(params.id)}
-        >
-          <DeleteForeverIcon color='error' />
+      sortable: false,
+      renderCell: params => (
+        <IconButton aria-label="Excluir" onClick={() => handleDeleteButtonClick(params.id)}>
+          <DeleteForeverIcon color="error" />
         </IconButton>
-      ),
+      )
     },
   ]
 
   const [state, setState] = React.useState({
-    cars: [],
+    customers: []
   })
-  const { cars } = state
+  const {
+    customers
+  } = state
 
   const { askForConfirmation, ConfirmDialog } = useConfirmDialog()
   const { notify, Notification } = useNotification()
   const { showWaiting, Waiting } = useWaiting()
 
   /*
-    useEffect() com vetor de dependências vazio irá ser executado
-    apenas uma vez, durante o carregamento inicial do componente
+    useEffect() com vetor de dependências vazio, para ser executado
+    uma única vez durante o carregamento inicial do componente e
+    disparar uma requisição ao back-end solicitando os dados a serem
+    exibidos
   */
   React.useEffect(() => {
     fetchData()
   }, [])
 
   async function fetchData() {
-    showWaiting(true)
+    // Exibe a tela de espera
+    showWaiting()
     try {
-      const result = await myfetch.get('/cars')
-      setState({
-        ...state,
-        cars: result,
-      })
-    } catch (error) {
+      const result = await myfetch.get('/customers?by=name')
+      
+      // Coloca o resultado no vetor customers
+      setState({ ...state, customers: result })
+    }
+    catch(error) {
       console.error(error)
-      notify(error.message, 'error')
-    } finally {
+      notify('ERRO: ' + error.message, 'error')
+    }
+    finally {
+      // Oculta a tela de espera
       showWaiting(false)
     }
   }
 
   async function handleDeleteButtonClick(deleteId) {
-    if (await askForConfirmation('Deseja realmente excluir este item?')) {
-      // Exibe a tela de espera
-      showWaiting(true)
+    if(await askForConfirmation('Deseja realmente excluir este item?', 'Confirmar operação')) {
+      showWaiting()   // Exibe a tela de espera
       try {
-        await myfetch.delete(`/cars/${deleteId}`)
+        // Efetua uma chamada ao back-end para tentar excluir o item
+        await myfetch.delete(`/customers/${deleteId}`)
 
         // Recarrega os dados da grid
         fetchData()
 
         notify('Item excluído com sucesso.')
-      } catch (error) {
-        console.log(error)
-        notify(error.message, 'error')
-      } finally {
-        showWaiting(false)
+      }
+      catch(error) {
+        console.error(error)
+        notify('ERRO: ' + error.message, 'error')
+      }
+      finally {
+        showWaiting(false)  // Ocultar a tela de espera
       }
     }
   }
 
-  return (
+  return(
     <>
       <Waiting />
+
       <Notification />
+
       <ConfirmDialog />
 
-      <Typography variant='h1' gutterBottom>
-        Listagem de carros
+      <Typography variant="h1" gutterBottom>
+        Listagem de clientes
       </Typography>
 
       <Box
         sx={{
           display: 'flex',
           justifyContent: 'right',
-          mb: 2, // marginBottom
+          mb: 2     // Margem inferior
         }}
       >
-        <Link to='./new'>
+        <Link to="./new">
           <Button
-            variant='contained'
-            size='large'
-            color='secondary'
+            variant="contained"
+            color="secondary"
+            size="large"
             startIcon={<AddBoxIcon />}
           >
-            Novo Carro
+            Novo cliente
           </Button>
         </Link>
       </Box>
@@ -193,7 +176,7 @@ export default function CarList() {
       <Paper elevation={10}>
         <Box sx={{ height: 400, width: '100%' }}>
           <DataGrid
-            rows={cars}
+            rows={customers}
             columns={columns}
             initialState={{
               pagination: {
@@ -205,7 +188,7 @@ export default function CarList() {
             pageSizeOptions={[5]}
           />
         </Box>
-      </Paper>
+      </Paper>     
     </>
   )
 }
